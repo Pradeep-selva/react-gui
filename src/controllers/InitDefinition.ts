6ff1ac8d038f9a34ace6c64fc0ccbbae9c77092c
pradeep-selva <pradeepselvaraju@gmail.com>
2021-01-28 19:10:36 +0530

    react-gui.init-def changes; error handing + content

--- src/controllers/InitDefinition.ts
+++ src/controllers/InitDefinition.ts
@@ -4,10 +4,35 @@ export default () => {
   const editor = vscode.window.activeTextEditor;
   const selection = editor?.selection;
   const text = editor?.document.getText(selection);
+  const path = vscode.window.activeTextEditor?.document.uri.fsPath;
+  console.log(Math.random() * 9);
+
+  if (
+    !["js", "ts", "jsx", "tsx"].includes(
+      path?.slice(path?.length - 2) || `${Math.random() * 9}`
+    )
+  ) {
+    vscode.window.showErrorMessage(
+      "Command can only be used in js, ts, jsx and tsx files."
+    );
+    return;
+  }
 
   const propList = text
     ?.split(",")
-    .map((prop) => prop.replace(" ", "").replace("\n", ""));
+    .map((prop) => `${prop.replace(" ", "").replace("\n", "")}: ;`)
+    .join("\n  ");
+
+  const content = `// replace 'Component' with the name of your component and add types to your props.
+import React from "react";
+
+interface ComponentProps {
+  ${propList}
+}
+
+declare const Component: React.SFC<ComponentProps>;
+
+export default Component;`;
 
   vscode.window.showInformationMessage(JSON.stringify(propList));
 };
